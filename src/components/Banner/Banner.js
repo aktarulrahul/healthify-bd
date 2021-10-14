0dcf53d46ad06f28ac5b0ec5de69c34e0ccc227d
aktarulrahul <contact.aktarul@gmail.com>
2021-10-14 20:14:49 +0600

    banner & subscription section done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './Banner.css';
 
-const Banner = () => {
+const Banner = ({ img, title, sub, btn, placeholder }) => {
   return (
-    <div>
-      <h2>Banner</h2>
+    <div className="m-4 hbd-banner-bg rounded-lg flex justify-between items-center px-6">
+      <div>
+        <h1 className="text-5xl mb-3 font-extrabold text-gray-800">{title}</h1>
+        <p className="text-gray-500 text-3xl mb-5">{sub}</p>
+        <div className="rounded-full bg-white flex justify-between">
+          <input
+            className="rounded-full py-3 px-6 focus:outline-none focus:border-transparent"
+            type="text"
+            placeholder={placeholder}
+          />
+          <button className="bg-green-400 text-white rounded-full py-3 px-6 transition duration-500 ease-in-out transform hover:scale-110 ">
+            {btn}
+          </button>
+        </div>
+      </div>
+      <div>
+        <img
+          className="transition duration-500 ease-in-out transform  hover:-translate-y-1 hover:scale-110 pl-4 h-full"
+          src={img}
+          alt={title}
+        />
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import Deals from '../../components/Deals/Deals';
-import Header from '../../components/Header/Header';
 import ProductContainer from '../../components/ProductContainer/ProductContainer';
-import Subscribe from '../../components/Subscribe/Subscribe';
 import WhyUs from '../../components/WhyUS/WhyUs';
+import banner from '../../images/Image/banner.png';
+import subscribe from '../../images/Image/subs.png';
 
 const Home = () => {
   return (
     <div>
-      <Banner />
+      <Banner
+        img={banner}
+        title="Best Food Waiting for your belly"
+        sub="Save up to 50% on your first order"
+        btn="Search"
+        placeholder="Search"
+      />
       <ProductContainer />
       <Deals />
-      <Subscribe />
+      <Banner
+        img={subscribe}
+        title="Stay home & get your food"
+        sub="Start your ordering"
+        btn="Subscribe"
+        placeholder="Your email"
+      />
       <WhyUs />
     </div>
   );
