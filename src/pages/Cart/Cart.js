f4e495f7c6129a26d4b06d0a95e09328f7d9214a
aktarulrahul <contact.aktarul@gmail.com>
2021-10-16 00:54:41 +0600

    shipping and cart page design done

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
   return (
-    <div>
-      <h2>cart</h2>
+    <div className="my-3 md:grid md:grid-cols-3 md:gap-4">
+      <div className="col-span-2 mx-auto mb-3">
+        <h2 className="text-2xl font-medium mb-3 ">Product Details</h2>
+      </div>
+      <div className="mx-auto mb-3">
+        <h2 className="text-2xl font-medium mb-3 ">Order Summary</h2>
+        <table className="table-auto">
+          <tbody>
+            <tr>
+              <td>Sub Total * 4 Item</td>
+              <td>$</td>
+            </tr>
+            <tr>
+              <td>Tax</td>
+              <td>$</td>
+            </tr>
+            <tr>
+              <td>Delivery Fee</td>
+              <td>$</td>
+            </tr>
+            <tr>
+              <td>Total</td>
+              <td>$</td>
+            </tr>
+          </tbody>
+        </table>
+        <Link to="/shipping">
+          <button className="p-2 mx-auto text-white bg-green-400 my-3 rounded">
+            Proceed to Shipping
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/pages/Shipping/Shipping.js
+++ src/pages/Shipping/Shipping.js
@@ -1,12 +1,56 @@
 import React from 'react';
-
+import logo from '../../images/logo/logo.png';
 const Shipping = () => {
   return (
-    <div>
-      <h2>Shipping</h2>
+    <div className="login-bg py-1">
+      <div className="md:w-1/3 px-2 bg-green-50 rounded shadow-lg mx-auto">
+        <img src={logo} className="w-full mx-auto mb-5" alt="" />
+        <h2 className="text-center text-2xl font-medium mb-3 ">
+          Delivery Details
+        </h2>
+        <form>
+          <input
+            type="text"
+            placeholder="Name"
+            className="border border-gray-400 rounded p-2 bg-gray-100 w-full mb-3"
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Phone"
+            className="border border-gray-400 rounded p-2 bg-gray-100 w-full mb-3"
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="House, Road"
+            className="border border-gray-400 rounded p-2 bg-gray-100 w-full mb-3"
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Flat or Floor"
+            className="border border-gray-400 rounded p-2 bg-gray-100 w-full mb-3"
+          />
+          <br />
+
+          <textarea
+            placeholder="Add Delivery Instruction"
+            cols="30"
+            rows="3"
+            className="border border-gray-400 rounded p-2 bg-gray-100 w-full mb-3"
+          ></textarea>
+          <br />
+          <button
+            type="submit"
+            className="border border-green-400 rounded p-2 bg-green-400 w-full mb-3 text-white"
+          >
+            Place Order
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
 
 export default Shipping;
-<h2>Shipping</h2>;
